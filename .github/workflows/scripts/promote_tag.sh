6d722ae0b5eba375760040a39f30c46f357b7ca0
DedlySpyder <dedlyspyder@gmail.com>
2023-01-01 21:50:03 -0500

    Added promote script

---
+++ .github/workflows/scripts/promote_tag.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+tag=$(echo "$GITHUB_REF" | cut -d '/' -f 3)
+_tag=$(echo "$tag" | cut -d 'v' -f 2)
+major="$(echo "$_tag" | cut -d '.' -f 1)"
+minor="$(echo "$_tag" | cut -d '.' -f 2)"
+
+git tag -d "v$major.$minor" 2> /dev/null
+git tag "v$major.$minor" "$GITHUB_SHA"
+
+git tag -d "v$major" 2> /dev/null
+git tag "v$major" "$GITHUB_SHA"
